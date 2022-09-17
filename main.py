a29cace635c5bc6855eeeae753fe2d0fc2c9d864
bluegleam0101 <sennasoeditos2@gmail.com>
2022-09-17 16:52:28 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,112 @@
+import time
+import csv
+from datetime import datetime
+import astropy.units as units
+from astropy.time import Time
+import geocoder
+from astropy.coordinates import SkyCoord, EarthLocation, AltAz, BaseRADecFrame
+from motor_controller import TelescopeMotorController, AltitudeMotor, AzimuthMotor
+from RpiMotorLib import RpiMotorLib
+
+
+class TelescopePointer:
+    def __init__(self, telescope_motor_api):
+        self.target: object
+
+    def set_target(self, query, latlng):
+        """gets current ra, dec, distance, altitude and azimuth for a celestial object specified by the 'celestial_body' parameter,
+         sets target. target astrometrics (ra, dec) given in floating point numbers which are degrees"""
+
+        time = Time(datetime.now())
+        geo_location = EarthLocation(lat=latlng[0]*units.deg, lon=latlng[0]*units.deg, height=5*units.m)
+
+        self.target = SkyCoord.from_name(query)
+        self.target = self.target.transform_to(AltAz(obstime=time,location=geo_location))
+        print(self.target.az, self.target.alt, self.target.info)
+        print(
+            f"right inclination axis: {self.target.transform_to(BaseRADecFrame(obstime=time)).ra} degrees\n"
+            f"declination axis: {self.target.transform_to(BaseRADecFrame(obstime=time)).dec} degrees\n"
+            #f"distance: {self.target['dis']}\n"
+            f"local altitude: {self.target.alt}\n"
+            f"local azimuth: {self.target.az}\n"
+        )
+
+    # align func is a function you have to pass that takes a ra- and dec axis and points the telescopes
+
+    def align(self, az, alt, telescope_motor_api, continuous=False, continuous_interval=10):
+        """
+        Aligns telescope by calling upon the telescope_motor_api function (that you have to create yourself).
+
+        takes ra and dec, both in degrees.
+
+        Set continuous to True if you want to keep aligning at an interval of 10 seconds. (10 is default but can be
+        changed using the continuous_interval parameter)
+        """
+        if not continuous:
+            telescope_motor_api.az_motor.align_azimuth(target_az=az)
+            print("aligning was successful")
+
+        if continuous:
+            print("continuously aligning...\nssss")
+            while True:
+                telescope_motor_api()
+                print('aligned')
+                time.sleep(continuous_interval)
+
+    def display_text(self, text):
+        pass
+
+# initializing objects #
+
+
+telescope_motor_api = TelescopeMotorController(az_motor=AzimuthMotor(
+                                                    rpimotor_object=RpiMotorLib.A4988Nema(
+                                                        direction_pin=26,
+                                                        step_pin=19,
+                                                        mode_pins=(21, 21, 21),
+                                                        motor_type="DRV8825"
+                                                                                    ),
+                                                    steps_360=200,
+                                                    gear_ratio=3,
+                                                                ),
+                                               alt_motor=AltitudeMotor())
+
+
+telescope_pointer = TelescopePointer(telescope_motor_api=telescope_motor_api)
+
+
+if __name__ == "__main__":
+    while True:
+        latlng = geocoder.ip('me').latlng
+        print(f"Current coordinates: lat: {latlng[0]}, long: {latlng[1]}.")
+        if latlng is None:
+            print("Could not get current coordinates.")
+            current_long = str.title(input("please specify your longitude: "))
+            current_lat = str.title(input("please specify your latitude: "))
+            latlng = [current_lat, current_long]
+
+        choice = input("would you like to set a new target or align telescope? (S/A): ")
+
+        if choice == 'S':
+            for i in range(1, 100):
+                try:
+                    telescope_motor_api.set_target(query=input("Please enter celestial object to set as target: "), latlng=latlng)
+                    print("target set, ready to align")
+                except ValueError:
+                    print("Celestial object not found, please try again.")
+                    continue
+                break
+
+        elif choice == 'A':
+            continuous = bool(int(input("Continuous align? 1 for yes, 0 for no: ")))
+            telescope_pointer.align(
+                alt=telescope_pointer.target.alt,
+                az=telescope_pointer.target.az,
+                continuous=continuous,
+                telescope_motor_api=telescope_motor_api
+            )
+
+    # pointer.align(dgbsdhfb
+
+
+
---
+++ motor_controller.py
@@ -0,0 +1,90 @@
+import time
+
+from RpiMotorLib import RpiMotorLib
+
+
+alt_pins = (17, 18, 27, 22)
+direction = 16
+step = 23
+EN_pin = 24
+
+
+class AltitudeMotor:
+    def __init__(self, rpimotor_function, gpiopins, steps_360, inv=False, wait=0.003, gear_ratio=1,
+                 steptype="full"):
+        '''
+        please provide gear ratio as a float corresponding to
+        the amount of times to motor has to turn 360° to fully
+        rotate to telescope once.
+
+        the steps_360 parameter asks for the amount of steps your stepper motor has to turn to turn 360° in full step mode
+        '''
+        self.name = rpimotor_function.name
+        self.current_position = 0.0
+        self.rpimotor_function = rpimotor_function
+        self.ccwise = False
+        self.degrees_to_turn: float
+        self.steps: int
+
+        # self.altitude_motor = RpiMotorLib.BYJMotor()
+        # self.altitude_motor.motor_run(gpiopins=alt_pins, wait=.003, steps=1536, ccwise=False,
+        # verbose=False, steptype="full", initdelay=.001)
+        def align_altitude():
+            pass
+
+
+class AzimuthMotor:
+    def __init__(self, rpimotor_object, steps_360, inv=False, wait=0.003, gear_ratio=1, steptype="full"):
+        '''
+        please provide gear ratio as a float corresponding to
+        the amount of times to motor has to turn 360° to fully
+        rotate to telescope once.
+
+        the steps_360 parameter asks for the amount of steps your stepper motor has to turn to turn 360° in full step mode
+        '''
+        self.name = rpimotor_object.name
+        self.current_position = 0.0
+        self.rpimotor_object = rpimotor_object
+        self.clockwise = False
+        self.degrees_to_turn: float
+        self.steps: int
+
+    def align_azimuth(self, target_az):
+        # finding delta degrees
+        a = target_az - self.current_position
+        a = (a + 180) % 360 - 180
+        self.degrees_to_turn = a
+
+        self.steps = int(((a / 360) * self.steps_360) * self.gear_ratio)
+        print(self.degrees_to_turn)
+
+        self.rpimotor_object.motor_go(clockwise=self.clockwise, steptype="Full", steps=self.steps, stepdelay=.005,
+                                      initdelay=0.1)
+
+
+class TelescopeMotorController():
+    def __init__(self, alt_motor, az_motor):
+        self.az_motor = az_motor
+        self.alt_motor = alt_motor
+
+        # self.rpimotor_function()
+
+###########################s
+# Actual motor control
+
+# GPIO.setmode(GPIO.BOARD)
+# GPIO.setup(EN_pin, GPIO.OUT)
+# x = 0
+# for i in range(0,2):
+#    GPIO.output(EN_pin, GPIO.LOW)  #s pull enable to low to enable motor
+#   mymotortest.motor_go(False,  # True=Clockwise, False=Counter-Clockwise
+#                          "Full",  # Step type (Full,Half,1/4,1/8,1/16,1/32)
+#                         200,  # number of steps
+#                        .0005,  # step delay [sec]
+#                       False,  # True = print verbose output
+#                      .05)  # initial delay [sec]
+# time.sleep(5)
+
+
+# GPIO.cleanup()  # clear GPIO allocations after run
+# skskksksksk
