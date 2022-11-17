0cb71337e609bee0f49f640839aaeb1c9ff178f9
bluegleam0101 <sennasoeditos2@gmail.com>
2022-11-17 12:32:54 +0100

    fix dms conversion to float

--- motor_controller.py
+++ motor_controller.py
@@ -51,7 +51,7 @@ class AltitudeMotor:
 
 
 class AzimuthMotor:
-    def __init__(self, rpimotor_object, steps_360, gpiopins=None, inv=False, wait=0.1, gear_ratio=1, steptype="Full",
+    def __init__(self, rpimotor_object, steps_360, gpiopins=None, inv=False, wait=0.05, gear_ratio=1, steptype="Full",
                  rpimotorlib_discrepancy=False):
         """
         please provide gear ratio as a float corresponding to
